993afa8790ba3a2213ee02d569ba60856e08bc81
pjonatansr <this.pablo@gmail.com>
2025-03-14 02:11:51 -0300

    fix hour param in workflow

--- scripts/getMessage.js
+++ scripts/getMessage.js
@@ -1,13 +1,36 @@
 const fs = require('fs');
 
 function getMessage(hour) {
-  const path = './messages.json'; // Caminho para o arquivo messages.json na raiz
-  const messages = JSON.parse(fs.readFileSync(path, 'utf-8'));
+  const path = './messages.json';
+  const defaultMentions = "<\@262002263101407233> <\@365676086874603522>\\n";
+  const defaultMessage = defaultMentions + 'MVP, bora!';
 
-  // Se houver uma mensagem para a hora atual, retorna ela, senão, retorna uma mensagem padrão
-  return messages[hour] || 'MVP, bora!';
+  if (!hour) {
+    return defaultMessage + ' (hora não informada)';
+  }
+
+  if (!fs.existsSync(path)) {
+    return defaultMessage + ' (arquivo não encontrado)';
+  }
+
+  const rawData = fs.readFileSync(path, 'utf-8');
+
+  let messages;
+  try {
+    messages = JSON.parse(rawData);
+  } catch (error) {
+    return defaultMessage + ' (erro ao fazer parse do JSON)';
+  }
+
+  if (!messages[hour]) {
+    return defaultMessage + ' (mensagem não encontrada)';
+  }
+
+  return `${defaultMentions}${messages[hour]}`;
 }
 
-// Para uso no GitHub Action
 const currentTime = process.env.time;
-console.log(getMessage(currentTime)); // Imprime a mensagem para o GitHub Action
+console.log(getMessage(currentTime));
+
+
+module.exports = getMessage;
