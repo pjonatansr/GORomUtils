652a92de5fd33315cc207786240290e99c9ba122
pjonatansr <this.pablo@gmail.com>
2025-03-14 02:16:30 -0300

    test: fix test

--- scripts/getMessage.test.js
+++ scripts/getMessage.test.js
@@ -8,12 +8,12 @@ describe('getMessage', () => {
 
 	test('returns a default message when hour is not in messages', () => {
 		const result = getMessage("11");
-		expect(result).toBe('<@262002263101407233> <@365676086874603522>\nMVP, bora! (mensagem não encontrada)');
+		expect(result).toBe('<@262002263101407233> <@365676086874603522>\\nMVP, bora! (mensagem não encontrada)');
 	});
 
 	test('returns a specific message when hour is in messages', () => {
 		const result = getMessage("10");
-		expect(result).toBe('<@262002263101407233> <@365676086874603522>\nSegunda wave de MVP iniciando! Vamos com tudo!');
+		expect(result).toBe('<@262002263101407233> <@365676086874603522>\\nSegunda wave de MVP iniciando! Vamos com tudo!');
 	});
 
 	test('reads messages from the correct file path', () => {
