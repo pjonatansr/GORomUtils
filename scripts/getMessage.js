ead4ce3390641aa99d3294e6f9f68bf9e7168b36
pjonatansr <this.pablo@gmail.com>
2025-03-14 01:39:33 -0300

    remove mentions

--- scripts/getMessage.js
+++ scripts/getMessage.js
@@ -4,10 +4,9 @@ function getMessage(hour) {
   const path = './messages.json';
   const messages = JSON.parse(fs.readFileSync(path, 'utf-8'));
   
-  const mentions = "<@262002263101407233> <@365676086874603522>"
   const message = (messages && messages.hasOwnProperty(hour)) ? messages[hour] : 'MVP, bora!';
 
-  return mentions + ' - ' + message;
+  return message;
 }
 
 module.exports = getMessage;
