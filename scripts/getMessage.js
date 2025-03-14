5cad71ff5d732fddd09e4d93e390537e1d39b79f
pjonatansr <this.pablo@gmail.com>
2025-03-14 01:40:54 -0300

    add mentions

--- scripts/getMessage.js
+++ scripts/getMessage.js
@@ -4,9 +4,12 @@ function getMessage(hour) {
   const path = './messages.json';
   const messages = JSON.parse(fs.readFileSync(path, 'utf-8'));
   
-  const message = (messages && messages.hasOwnProperty(hour)) ? messages[hour] : 'MVP, bora!';
+  const mentions = "<@262002263101407233> <@365676086874603522>"
+  if (messages[hour] === undefined) {
+    return `${mentions}\nMVP, bora!`;
+  }
 
-  return message;
+  return `${mentions}\n${messages[hour]}`;
 }
 
 module.exports = getMessage;
