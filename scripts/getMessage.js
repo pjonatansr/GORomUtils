114648180009fd82a42da2fa81dfe8fe3480d050
pjonatansr <this.pablo@gmail.com>
2025-04-09 21:10:11 -0300

    feat: remove mentions

--- scripts/getMessage.js
+++ scripts/getMessage.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function getMessage(hour) {
   const path = './messages.json';
-  const defaultMentions = "<\@262002263101407233> <\@365676086874603522>\\n";
+  const defaultMentions = "" //"<\@262002263101407233> <\@365676086874603522>\\n";
   const defaultMessage = defaultMentions + 'MVP, bora!';
 
   if (!hour) {
